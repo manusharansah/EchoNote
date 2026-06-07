98734d10ea4174f133a49fdfd880e955e5c36e31
dilasha68 <dilashaadhikari20@gmail.com>
2026-06-07 08:25:53 +0545

    remove old frontends

--- frontend-new/src/components/auth/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import useAuthStore from '../../store/authStore'
-
-export default function ProtectedRoute({ children }) {
-  const { token, initialized } = useAuthStore()
-  if (!initialized) return null  // splash / loading handled in App
-  if (!token) return <Navigate to="/signin" replace />
-  return children
-}
